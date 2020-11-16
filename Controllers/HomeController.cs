59cb8eba1a6cfbc7f1d7a7edb30a6c289b050f8c
Vyshnivskyi Vladyslav <vyshnivskyivlad@gmail.com>
2020-11-16 20:58:09 +0200

    Add null-verifications in Controller for model fields

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
 
 namespace LanguageFeatures.Controllers
 {
@@ -6,7 +8,15 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            return View(new string[] { "C#", "Language", "Features" });
+            List<string> results = new List<string>();
+            foreach(Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<NoName>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+            }
+            return View(results);        
         }
     }
 }
--- Models/Product.cs
+++ Models/Product.cs
@@ -6,6 +6,7 @@ namespace LanguageFeatures.Models
     {
         public string Name { get; set; }
         public decimal? Price { get; set; }
+        public Product Related { get; set; }
         public static Product[] GetProducts()
         {
             Product kayak = new Product { 
@@ -16,6 +17,8 @@ namespace LanguageFeatures.Models
                 Name = "Lifejacket",
                 Price = 48.95M
             };
+
+            kayak.Related = lifejacket;
 
             return new Product[] { kayak, lifejacket, null };
 
