5b72f5810df4cfdbae5b83608cd9a334d241ba81
Vyshnivskyi Vladyslav <vyshnivskyivlad@gmail.com>
2020-11-18 14:42:06 +0200

    Some variants of using C# syntax sugar.

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,15 +4,23 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
+        public Product(bool stock = true)
+        {
+            InStock = stock;
+        }
         public string Name { get; set; }
         public decimal? Price { get; set; }
+        public string Category { get; set; } = "Watersports";
         public Product Related { get; set; }
+        public bool InStock { get; }
         public static Product[] GetProducts()
         {
             Product kayak = new Product { 
-                Name = "Kayak", Price = 275M
+                Name = "Kayak",
+                Price = 275M,
+                Category = "Water Craft"
             };
-            Product lifejacket = new Product
+            Product lifejacket = new Product(false)
             {
                 Name = "Lifejacket",
                 Price = 48.95M
