0bbb3420e07452109e89a6ae530d49142616237d
Vyshnivskyi Vladyslav <vyshnivskyivlad@gmail.com>
2020-11-18 16:13:07 +0200

    Bug fixes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,20 +8,25 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
-            decimal total = 0;
-            for (int i = 0; i < data.Length; i++)
-            {
-                switch (data[i])
-                {
-                    case decimal decimalValue:
-                        total += decimalValue;
-                        break;
-                    case int intValue when intValue > 50:
-                        total += intValue;
-                        break;
-                }
-            }
+            ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
+            decimal total = cart.TotalPrices();
+
+            #region switch
+            //object[] data = new object[] { 275M, 29.95M, "apple", "orange", 100, 10 };
+            //decimal total = 0;
+            //for (int i = 0; i < data.Length; i++)
+            //{
+            //    switch (data[i])
+            //    {
+            //        case decimal decimalValue:
+            //            total += decimalValue;
+            //            break;
+            //        case int intValue when intValue > 50:
+            //            total += intValue;
+            //            break;
+            //    }
+            //}
+            #endregion
             #region if
             //for (int i = 0; i < data.Length; i++)
             //{
---
+++ Models/MyExtensionMethod.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace LanguageFeatures.Models
+{
+    public static class MyExtensionMethod
+    {
+        public static decimal TotalPrices(this ShoppingCart cartParam)
+        {
+            decimal total = 0;
+            foreach (Product prod in cartParam.Products)
+            {
+                total += prod?.Price ?? 0;
+            }
+            return total;
+        }
+    }
+}
